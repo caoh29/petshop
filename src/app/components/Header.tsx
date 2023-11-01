5a1b38e83dc87da14aedb2a5f9aed76c1cf8c994
Camilo Ordoñez <cronox20@gmail.com>
2023-10-31 21:24:44 -0400

    addin navbar

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,10 +1,13 @@
-"use client";
-import { useState } from "react";
-import Link from "next/link";
+'use client';
+import { useState } from 'react';
+import Link from 'next/link';
 
-import { useCart } from "../store/store";
-import CartPopup from "./CartPopup";
-import { type Cart } from "@/api/types";
+import { useCart } from '../store/store';
+
+import CartPopup from './CartPopup';
+import { type Cart } from '@/api/types';
+
+import NavBar from './NavBar';
 
 export default function Header({
   clearCartAction,
@@ -15,23 +18,24 @@ export default function Header({
   const [showCart, setShowCart] = useState(false);
 
   return (
-    <header className="mx-2 flex items-center justify-between p-4 bg-blue-800 mb-10 shadow-lg shadow-white rounded-b-2xl">
-      <Link href="/">
-        <h1 className="text-3xl font-bold leading-10 text-gray-100">
-          Donuts &amp; Dragoons Store
-        </h1>
+    <header className='flex items-center justify-between p-4 bg-teal-900 mb-10 h-32'>
+      <Link href='/'>
+        <h2 className='scroll-m-20 pb-2 text-3xl font-semibold text-amber-500 tracking-tight first:mt-0'>
+          PetShop
+        </h2>
       </Link>
       <div
-        className="flex items-center justify-center w-10 h-10 bg-blue-700 rounded-full"
+        className='flex items-center justify-center w-10 h-10 bg-blue-700 rounded-full'
         onClick={() => {
           setShowCart(!showCart);
         }}
       >
-        <span className="text-xl font-bold leading-10 text-gray-100">
+        <span className='text-xl font-bold leading-10 text-gray-100'>
           {cart.products.length}
         </span>
         {showCart && <CartPopup clearCartAction={clearCartAction} />}
       </div>
+      <NavBar />
     </header>
   );
 }

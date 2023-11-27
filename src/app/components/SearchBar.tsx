158da80f5bf48940962b50f38a76d416fe3e7313
Camilo Ordoñez <cronox20@gmail.com>
2023-11-27 10:58:47 -0500

    adding functionality to search bar

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useState } from 'react';
 
 import { Search } from 'lucide-react';
 import {
@@ -12,7 +13,17 @@ import {
   SheetTrigger,
 } from './ui/sheet';
 
+import Data from '../../mocks/MOCK_DATA.json';
+
+type Post = {
+  id: number;
+  product_name: string;
+  category: string;
+  sku: number;
+};
+
 export function SearchBar() {
+  const [query, setQuery] = useState('');
   return (
     <div className='grid grid-cols-2 gap-2'>
       <Sheet>
@@ -20,17 +31,33 @@ export function SearchBar() {
           <Search color='white' className='hover:scale-110' />
         </SheetTrigger>
         <SheetContent side='top'>
-          <div className='grid gap-4 py-4'>
-            <div className='grid grid-cols-4 items-center gap-4'>
-              <label htmlFor='name' className='text-right'>
-                Search
-              </label>
-              <input id='name' type='text' className='col-span-3' />
-            </div>
+          <div className='flex justify-center gap-4'>
+            <label htmlFor='name' className='text-right'>
+              Search
+            </label>
+            <input
+              id='name'
+              type='text'
+              className='w-4/5'
+              onChange={(e) => setQuery(e.target.value)}
+            />
           </div>
+          {query &&
+            Data.filter((post: Post) => {
+              if (
+                post.product_name.toLowerCase().includes(query.toLowerCase())
+              ) {
+                return post;
+              }
+            }).map((post, index) => (
+              <div className='box' key={post.id}>
+                <p>{post.product_name}</p>
+                <p>{post.category}</p>
+              </div>
+            ))}
           <SheetFooter>
             <SheetClose asChild>
-              <button>Submit</button>
+              <button className='mt-5 mr-6 bg-black text-white'>Submit</button>
             </SheetClose>
           </SheetFooter>
         </SheetContent>

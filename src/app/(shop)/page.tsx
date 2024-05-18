e2a5389f344db41d2205449f5d984964bc903ff5
Camilo Ordoñez <cronox20@gmail.com>
2024-05-18 13:35:07 -0400

    creating pages, integrate products inside app to products inside shop

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   const products = await getProducts();
   return (
     <div className='flex flex-wrap gap-2'>
-      <ul role='list' className='flex flex-row flex-wrap m-2'>
+      <ul className='flex flex-row flex-wrap m-2'>
         {products.map((product) => (
           <li key={product.id} className='md:w-1/3'>
             <Link href={`/products/${product.id}`}>

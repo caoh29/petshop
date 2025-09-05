4af15befd5427880c7b13738a4dfc20a65e28e2c
caoh29 <cronox20@hotmail.com>
2025-09-04 23:29:33 -0400

    modifying best seler page just added h1 tag and p tag nothing more

--- src/app/(shop)/best-sellers/page.tsx
+++ src/app/(shop)/best-sellers/page.tsx
@@ -1,3 +1,8 @@
 export default function BestSellersPage() {
-  return <div>Best Sellers</div>;
+  return (
+    <div>
+      <h1>Best Sellers</h1>
+      <p>List of best-selling products will be displayed here.</p>
+    </div>
+  );
 }
